ed513529a20381a054e5ff779412479b8612edc4
codewithprofessor <kalikali72547254@gmail.com>
2024-07-24 14:59:29 +0500

    entity create comment wo mapping

---
+++ src/main/java/com/blog/entites/Comment.java
@@ -0,0 +1,27 @@
+package com.blog.entites;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String comment;
+
+    @Column(nullable = false)
+    private Date commentDate;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false ;
+
+}
