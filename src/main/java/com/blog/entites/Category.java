83ad3bc0590a3606dbd471baca4a1ae9ee8aa572
codewithprofessor <kalikali72547254@gmail.com>
2024-07-24 15:03:10 +0500

    create Category without mapping

---
+++ src/main/java/com/blog/entites/Category.java
@@ -0,0 +1,26 @@
+package com.blog.entites;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false)
+    private Date assignDate;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
