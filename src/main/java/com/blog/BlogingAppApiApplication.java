245289f6bcf9b4f7dfc9dba68236e38c3be6e87a
codewithprofessor <kalikali72547254@gmail.com>
2024-07-24 12:43:42 +0500

    create project

---
+++ src/main/java/com/blog/BlogingAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingAppApiApplication.class, args);
+	}
+
+}
