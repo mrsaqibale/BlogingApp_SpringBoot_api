b64bf763d55f29c4a34fdd17418428a0231912dc
codewithprofessor <kalikali72547254@gmail.com>
2024-07-24 13:36:22 +0500

    complete user entity for user

--- src/main/java/com/blog/entites/User.java
+++ src/main/java/com/blog/entites/User.java
@@ -1,11 +1,42 @@
 package com.blog.entites;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String username;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column( nullable = false)
+	private String password;
+
+	@Column()
+	private String about;
+
+	@Column()
+	private String phNo;
+
+	@Column()
+	private String picture;
+
+	@Column( nullable = false)
+	private boolean isActive = true;
+
+	@Column( nullable = false)
+	private boolean isDeleted = false;
+
+
 }
