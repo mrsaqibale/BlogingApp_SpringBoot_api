3ac3466c92efc46d875f0249217c1229993e8632
codewithprofessor <kalikali72547254@gmail.com>
2024-08-01 00:15:13 +0500

    create modelmapper

--- src/test/java/com/blog/BlogingAppApiApplicationTests.java
+++ src/test/java/com/blog/BlogingAppApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.blog;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 class BlogingAppApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
