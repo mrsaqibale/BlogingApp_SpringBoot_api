3728bd892d63432a82292893e589a024787c54bc
codewithprofessor <kalikali72547254@gmail.com>
2024-08-07 22:30:46 +0500

    category dto created

--- src/main/java/com/blog/dto/CategoryDto.java
+++ src/main/java/com/blog/dto/CategoryDto.java
@@ -1,5 +1,24 @@
 package com.blog.dto;
 
+import java.sql.Date;
+import java.util.List;
+
+import com.blog.entites.Post;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+
 public class CategoryDto {
 
+    private Long id;
+
+    private String title;
+
+    private Date assignDate;
+
+    private boolean isDeleted = false;
+
+    // private List<Post> posts ;
 }
