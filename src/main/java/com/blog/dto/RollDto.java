1a672a0ccb727ab615f6d9c60af569a4eeb72bb0
codewithprofessor <kalikali72547254@gmail.com>
2024-08-08 22:19:11 +0500

    roll dto

--- src/main/java/com/blog/dto/RollDto.java
+++ src/main/java/com/blog/dto/RollDto.java
@@ -1,8 +1,19 @@
 package com.blog.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RollDto {
     
-    private String username;
+    private int id;
 
-    
+    private String name;
+
+    // private List<User> users;
 }
