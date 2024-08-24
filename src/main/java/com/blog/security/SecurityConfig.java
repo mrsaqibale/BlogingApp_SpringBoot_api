c67b7c221485fc862d3e08ef6d617be03c5b6cba
codewithprofessor <kalikali72547254@gmail.com>
2024-08-24 22:12:45 +0500

    update versions and sec

--- src/main/java/com/blog/security/SecurityConfig.java
+++ src/main/java/com/blog/security/SecurityConfig.java
@@ -1,5 +1,26 @@
 package com.blog.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
-    
+    /**
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+        csrf().disable().
+        authorizeRequests().
+        antMatchers("/login").permitAll().
+        anyRequest().authenticated().
+        and()
+        
+        return http.build();
+    }
 }
