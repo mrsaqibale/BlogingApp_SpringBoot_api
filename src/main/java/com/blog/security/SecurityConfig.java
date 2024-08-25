385ac44b064205f06937c6d278d1158366d213a2
codewithprofessor <kalikali72547254@gmail.com>
2024-08-25 18:55:49 +0500

    set security basic

--- src/main/java/com/blog/security/SecurityConfig.java
+++ src/main/java/com/blog/security/SecurityConfig.java
@@ -2,24 +2,22 @@ package com.blog.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    /**
-     * @param http
-     * @return
-     * @throws Exception
-     */
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.
-        csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests((request) -> request
+        http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(request -> request
         .requestMatchers("/home/**").permitAll()
-        .anyRequest().authenticated()
-        );
+        .anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
