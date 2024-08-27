c75a0f69b63019a8a4662f0acb35fba3e3108da7
codewithprofessor <kalikali72547254@gmail.com>
2024-08-27 09:54:46 +0500

    add jwt filter

---
+++ src/main/java/com/blog/filter/JwtFilter.java
@@ -0,0 +1,20 @@
+package com.blog.filter;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+    }
+    
+}
