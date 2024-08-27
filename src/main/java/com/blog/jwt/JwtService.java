8153bdec0d35613866bb0dcf79577d5725e93b02
codewithprofessor <kalikali72547254@gmail.com>
2024-08-27 21:54:18 +0500

    update jwtservice with errors

--- src/main/java/com/blog/jwt/JwtService.java
+++ src/main/java/com/blog/jwt/JwtService.java
@@ -8,49 +8,76 @@ import java.util.Map;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-
 
 @Service
 public class JwtService {
 
     // private String fkey;
-    public JwtService(){
+    public JwtService() {
         // try {
-        //     KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-        //     SecretKey sk = keyGen.generateKey();
-        //     fkey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        // KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+        // SecretKey sk = keyGen.generateKey();
+        // fkey = Base64.getEncoder().encodeToString(sk.getEncoded());
         // } catch (Exception e) {
-        //     throw new RuntimeException();
+        // throw new RuntimeException();
         // }
         // fkey = "R{!D6,wY=i9vcjqMwR2P6JUNG8~v7r1cBI=?Z=uUg/!NN>&v?0j%}z,DQ6eEc&";
-        
+
     }
+
     public String genrateToken(String username) {
-        Map<String , Object> claims = new HashMap<>();
+        Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
-            .claims()
-            .add(claims)
-            .subject(username)
-            .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis() + 60 *60 *10))
-            .and()
-            .signWith(getKey())
-            .compact();
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 10))
+                .and()
+                .signWith(getKey())
+                .compact();
     }
+
     private Key getKey() {
-        byte[] keyBytes = Decoders.BASE64.decode("ehllomanthisisthekeyandwhatisyourfirstnameisnofhtehishtheogoodofhtehis");
+        byte[] keyBytes = Decoders.BASE64
+                .decode("ehllomanthisisthekeyandwhatisyourfirstnameisnofhtehishtheogoodofhtehis");
         return Keys.hmacShaKeyFor(keyBytes);
     }
+
     public String extractUserName(String token) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'extractUserName'");
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
+        // return extractClaim(token, Claims::getSubject);
     }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
     public boolean validateToken(String token, UserDetails userDetails) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validateToken'");
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
-    
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
 }
